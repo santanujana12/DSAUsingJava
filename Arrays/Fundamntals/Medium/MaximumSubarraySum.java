d1381bc6823a0a8721e1b46ff6cddf7038672fb9
Santanu Jana <santanu.jana@aol.com>
2025-03-27 04:06:43 +0530

    Implementation of kadene algorithm

--- Arrays/Fundamntals/Medium/MaximumSubarraySum.java
+++ Arrays/Fundamntals/Medium/MaximumSubarraySum.java
@@ -18,8 +18,29 @@ public class MaximumSubarraySum {
         return maxResult;
     }
 
+    // Resolving the same with More optimal algorithm Kadene's algorithm
+    public static int KadeneAlgorithm(int arr[]){
+        int maxResult = -99999;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            // add current element to the sum
+            sum+=arr[i];
+            // check if maxResult is less than sum then update maxResult with sum
+            if(sum>maxResult){
+                maxResult=sum;
+            }
+            // reset sum to 0 if sum is found to be negative
+            if(sum<0){
+                sum=0;
+            }
+        }
+
+        return maxResult;
+    }
+
+
     public static void main(String[] args) {
         int arr[] = { -2, -3, -7, -2, -10, -4 };
-        System.out.println(maxSubarraySum(arr));
+        System.out.println(KadeneAlgorithm(arr));
     }
 }
