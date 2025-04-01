f54db283aa19ef80887859f76968dbdb8ae868d1
Santanu Jana <santanu.jana@aol.com>
2025-04-02 04:00:33 +0530

    Added Moore's voting algorithm

--- Arrays/Fundamntals/Hard/MajorityElementOne.java
+++ Arrays/Fundamntals/Hard/MajorityElementOne.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class MajorityElementOne {
 
+    // Space complexity O(n)
     public static int findMajorityElement(int[] arr) {
         int result = -1;
         int n = arr.length;
@@ -31,9 +32,40 @@ public class MajorityElementOne {
         return result;
     }
 
+    // further optimizing Moore's voting algorithm
+    public static int MooresVotingAlgorithm(int arr[]){
+        int element = 0;
+        int count = 0;
+        // checking if the current element is equal to next element then increase the count else decrease the count
+        // if count becomes equal to 0 then change the element
+        for(int i=0;i<arr.length;i++){
+            if(count==0){
+                count=1;
+                element=arr[i];
+            }else if(element!=arr[i]){
+                count--;
+            }else{
+                count++;
+            }
+        }
+
+        int finalCount=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==element){
+                finalCount++;
+            }
+        }
+
+        if(finalCount>=(arr.length/2)){
+            return element;
+        }
+        return 0;
+    }
+    
+
     public static void main(String[] args) {
         int arr[] = { 7, 0, 0, 1, 7, 7, 2, 7, 7 };
 
-        System.out.println(findMajorityElement(arr));
+        System.out.println(MooresVotingAlgorithm(arr));
     }
 }
