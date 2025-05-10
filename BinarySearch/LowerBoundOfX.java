8dc7f2395c7a16ebfe41ea2f655e4a16caaab2d4
Santanu Jana <santanu.jana@aol.com>
2025-05-11 01:23:04 +0530

    Added Upper Bound Of x

---
+++ BinarySearch/LowerBoundOfX.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class LowerBoundOfX {
+    public static int findLowerBound(int arr[], int k) {
+        Arrays.sort(arr);
+        int start = 0, end = arr.length - 1, minIndex = arr.length;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] >= k) {
+                minIndex = Math.min(minIndex, mid);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return minIndex;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3,5,8,15,19 };
+        int k = 3;
+        System.out.println(findLowerBound(arr, k));
+    }
+}
---
+++ BinarySearch/UpperBoundOfX.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+
+public class UpperBoundOfX {
+    public static int findUpperBound(int arr[], int k) {
+        int start = 0, end = arr.length - 1, minIndex = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] <= k) {
+                minIndex = Math.max(minIndex, mid);
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return minIndex+1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3,5,8,15,19 };
+        int k = 9;
+        System.out.println(findUpperBound(arr, k));
+    }
+}
