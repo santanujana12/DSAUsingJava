e35ad5903282df3e14f5f0b38f0355ef8970cb6e
santanujana12 <santanu.jana@aol.com>
2025-08-07 01:13:48 +0530

    Added MergeTwoSortedArraysleetCode

---
+++ Arrays/LogicBuilding/MergeTwoSortedArraysLeetCode.java
@@ -0,0 +1,27 @@
+package Arrays.LogicBuilding;
+
+public class MergeTwoSortedArraysLeetCode {
+
+    public static void mergeArrays(int nums1[], int m, int nums2[], int n) {
+        int pointer_a = m - 1, pointer_b = n - 1;
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (pointer_a >= 0 && (pointer_b < 0 || nums1[pointer_a] >= nums2[pointer_b])) {
+                nums1[i] = nums1[pointer_a];
+                pointer_a--;
+            } else if (pointer_b >= 0 && nums2[pointer_b] >= nums1[pointer_a]) {
+                nums1[i] = nums2[pointer_b];
+                pointer_b--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = {};
+        int nums2[] = {1};
+        int m = 0, n = 1;
+        mergeArrays(nums1, m, nums2, n);
+        for (int i : nums1) {
+            System.out.print(i + " ");
+        }
+    }
+}
