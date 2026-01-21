51fd31026573bbe21b5dbd4d527821942683928f
santanujana12 <santanu.jana@aol.com>
2026-01-21 13:46:21 +0530

    Add TreeNode and PreOrderTraversal classes for binary tree operations

---
+++ Trees/BinaryTrees/PreOrderTraversal.java
@@ -0,0 +1,33 @@
+package Trees.BinaryTrees;
+
+import java.util.ArrayList;
+
+public class PreOrderTraversal {
+
+    public static void preOrderHelper(TreeNode root, ArrayList<Integer>result){
+        if(root==null){
+            return;
+        }
+        result.add(root.data);
+        preOrderHelper(root.left,result);
+        preOrderHelper(root.right,result);
+    }
+
+    public static ArrayList<Integer> preOrderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        preOrderHelper(root, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        ArrayList<Integer> result = preOrderTraversal(root);
+
+        System.out.println("Pre-order Traversal: " + result);
+    }
+}
