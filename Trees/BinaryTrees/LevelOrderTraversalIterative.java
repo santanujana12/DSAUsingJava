6f87b6a513578580af913ebbcef5ea0d3b96baa2
santanujana12 <santanu.jana@aol.com>
2026-01-27 20:00:50 +0530

    Add LevelOrderTraversalIterative class for binary tree level order traversal

---
+++ Trees/BinaryTrees/LevelOrderTraversalIterative.java
@@ -0,0 +1,64 @@
+package Trees.BinaryTrees;
+
+import java.util.*;
+
+
+public class LevelOrderTraversalIterative {
+
+    public static List<List<Integer>> levelOrderTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.data);
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        // root.right.left is null
+        root.right.right = new TreeNode(8);
+
+        // children of node 5
+        root.left.right.left = new TreeNode(6);
+        root.left.right.right = new TreeNode(7);
+
+        // child of node 8
+        root.right.right.left = new TreeNode(9);
+
+        List<List<Integer>> result = levelOrderTraversal(root);
+
+        for (List<Integer> level : result) {
+            for (int val : level) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
